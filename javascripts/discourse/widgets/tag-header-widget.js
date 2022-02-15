ddef7ca2c9534da5967beb5fb5b7116a1624efab
awesomerobot <kris.aubuchon@discourse.org>
2022-02-15 16:19:11 -0500

    FEATURE: Show tag descriptions

--- javascripts/discourse/widgets/tag-header-widget.js
+++ javascripts/discourse/widgets/tag-header-widget.js
@@ -4,14 +4,28 @@ import { h } from "virtual-dom";
 
 export default createWidget("tag-header-widget", {
   tagName: "span",
+
+  buildKey: () => `tag-header-widget`,
+
+  defaultState() {
+    return { loaded: false, tag: null };
+  },
+
+  getTagInfo(tag) {
+    this.store.find("tag-info", tag).then((result) => {
+      this.state.tag = result;
+      this.state.loaded = true;
+      this.scheduleRerender();
+    });
+  },
+
   html() {
     const router = getOwner(this).lookup("router:main");
     const route = router.currentRoute;
+    const hideMobile =
+      !settings.show_on_mobile && this.site.mobileView ? "true" : hideMobile;
 
     if (route && route.params && route.params.hasOwnProperty("tag_id")) {
-      const hideMobile =
-        !settings.show_on_mobile && this.site.mobileView ? "true" : hideMobile;
-
       let tag = route.params.tag_id;
       let additionalTags = route.params.additional_tags;
 
@@ -27,6 +41,7 @@ export default createWidget("tag-header-widget", {
 
         let additionalTagNames;
         let additionalClass;
+        let tagDescription;
 
         if (additionalTags) {
           let tagList = additionalTags.split("/");
@@ -39,13 +54,28 @@ export default createWidget("tag-header-widget", {
           additionalClass = "single-tag";
         }
 
-        return h(
-          `div.tag-title-header .tag-banner-${tag} .${additionalClass}`,
+        if (!this.state.loaded) {
+          this.getTagInfo(tag);
+        } else {
+          if (this.state.tag.name != tag) {
+            // update the tag description when the route's tag changes
+            this.getTagInfo(tag);
+          }
 
-          h("div.tag-title-contents", [
-            h("h1", [h("span", tag), additionalTagNames]),
-          ])
-        );
+          if (additionalTags || !settings.show_tag_description) {
+            tagDescription = "";
+          } else {
+            tagDescription = this.state.tag.description;
+          }
+
+          return h(
+            `div.tag-title-header .tag-banner-${tag} .${additionalClass}`,
+            h("div.tag-title-contents", [
+              h("h1", [h("span", tag), additionalTagNames]),
+              h("p", tagDescription),
+            ])
+          );
+        }
       } else {
         document.querySelector("body").classList.remove("tag-banner");
       }
