59240bfe003730b938d9c73c20a61845965486ec
awesomerobot <kris.aubuchon@discourse.org>
2022-07-25 14:53:40 -0400

    Fix hyphen hiding, add underscore setting, lint

--- javascripts/discourse/widgets/tag-header-widget.js
+++ javascripts/discourse/widgets/tag-header-widget.js
@@ -1,84 +1,96 @@
-import { createWidget } from "discourse/widgets/widget";
-import { getOwner } from "discourse-common/lib/get-owner";
-import { h } from "virtual-dom";
+import { createWidget } from 'discourse/widgets/widget'
+import { getOwner } from 'discourse-common/lib/get-owner'
+import { h } from 'virtual-dom'
 
-export default createWidget("tag-header-widget", {
-  tagName: "span",
+export default createWidget('tag-header-widget', {
+  tagName: 'span',
 
   buildKey: () => `tag-header-widget`,
 
   defaultState() {
-    return { loaded: false, tag: null };
+    return { loaded: false, tag: null }
   },
 
   getTagInfo(tag) {
-    this.store.find("tag-info", tag).then((result) => {
-      this.state.tag = result;
-      this.state.loaded = true;
-      this.scheduleRerender();
-    });
+    this.store.find('tag-info', tag).then(result => {
+      this.state.tag = result
+      this.state.loaded = true
+      this.scheduleRerender()
+    })
   },
 
   html() {
-    const router = getOwner(this).lookup("router:main");
-    const route = router.currentRoute;
+    const router = getOwner(this).lookup('router:main')
+    const route = router.currentRoute
     const hideMobile =
-      !settings.show_on_mobile && this.site.mobileView ? "true" : hideMobile;
+      !settings.show_on_mobile && this.site.mobileView ? 'true' : hideMobile
 
-    if (route && route.params && route.params.hasOwnProperty("tag_id")) {
-      let tag = route.params.tag_id;
-      let additionalTags = route.params.additional_tags;
+    if (route && route.params && route.params.hasOwnProperty('tag_id')) {
+      let tag = route.params.tag_id
+      let formattedTagName = tag
+
+      let additionalTags = route.params.additional_tags
+      let formattedAdditionalTagNames = additionalTags
 
       if (settings.remove_tag_hyphen) {
-        tag = tag.replace(/-/g, " ");
-        additionalTags = additionalTags
-          ? additionalTags.replace(/-/g, " ")
-          : null;
+        formattedTagName = formattedTagName.replace(/-/g, ' ')
+        formattedAdditionalTagNames = formattedAdditionalTagNames
+          ? formattedAdditionalTagNames.replace(/-/g, ' ')
+          : null
       }
 
-      if (!hideMobile && tag != "none") {
-        document.querySelector("body").classList.add("tag-banner");
+      if (settings.remove_tag_underscore) {
+        formattedTagName = formattedTagName.replace(/_/g, ' ')
+        formattedAdditionalTagNames = formattedAdditionalTagNames
+          ? formattedAdditionalTagNames.replace(/_/g, ' ')
+          : null
+      }
 
-        let additionalTagNames;
-        let additionalClass;
-        let tagDescription;
+      if (!hideMobile && tag != 'none') {
+        document.querySelector('body').classList.add('tag-banner')
+
+        let additionalClass
+        let tagDescription
 
         if (additionalTags) {
-          let tagList = additionalTags.split("/");
+          let tagList = formattedAdditionalTagNames.split('/')
           let additionalClassList = tagList.map(function (e) {
-            return `tag-banner-${e}`;
-          });
-          additionalTagNames = h("span", ` & ${tagList.join(" & ")}`);
-          additionalClass = additionalClassList.join(".");
+            return `tag-banner-${e}`
+          })
+          formattedAdditionalTagNames = h('span', ` & ${tagList.join(' & ')}`)
+          additionalClass = additionalClassList.join('.')
         } else {
-          additionalClass = "single-tag";
+          additionalClass = 'single-tag'
         }
 
         if (!this.state.loaded) {
-          this.getTagInfo(tag);
+          this.getTagInfo(tag)
         } else {
           if (this.state.tag.name != tag) {
             // update the tag description when the route's tag changes
-            this.getTagInfo(tag);
+            this.getTagInfo(tag)
           }
 
           if (additionalTags || !settings.show_tag_description) {
-            tagDescription = "";
+            tagDescription = ''
           } else {
-            tagDescription = this.state.tag.description;
+            tagDescription = this.state.tag.description
           }
 
           return h(
             `div.tag-title-header .tag-banner-${tag} .${additionalClass}`,
-            h("div.tag-title-contents", [
-              h("h1", [h("span", tag), additionalTagNames]),
-              h("p", tagDescription),
+            h('div.tag-title-contents', [
+              h('h1', [
+                h('span', formattedTagName),
+                formattedAdditionalTagNames,
+              ]),
+              h('p', tagDescription),
             ])
-          );
+          )
         }
       } else {
-        document.querySelector("body").classList.remove("tag-banner");
+        document.querySelector('body').classList.remove('tag-banner')
       }
     }
   },
-});
+})
