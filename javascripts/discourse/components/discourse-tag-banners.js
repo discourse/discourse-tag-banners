9f375ef0e0e946f8e8b2585328ca905d5ec2ae96
chapoi <101828855+chapoi@users.noreply.github.com>
2024-01-18 12:21:00 +0200

    FIX: getOwner deprecation (#16)

--- javascripts/discourse/components/discourse-tag-banners.js
+++ javascripts/discourse/components/discourse-tag-banners.js
@@ -2,7 +2,7 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import { getOwner } from "discourse-common/lib/get-owner";
+import { getOwner } from "@ember/application";
 
 export default class DiscourseTagBanners extends Component {
   @service store;
