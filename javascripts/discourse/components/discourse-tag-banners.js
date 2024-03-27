47cbaa39f7645ad114f7929b6351034a1508aa5d
David Taylor <david@taylorhq.com>
2024-03-27 17:01:52 +0000

    DEV: Update linting

--- javascripts/discourse/components/discourse-tag-banners.js
+++ javascripts/discourse/components/discourse-tag-banners.js
@@ -1,8 +1,8 @@
 import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
-import { action } from "@ember/object";
-import { inject as service } from "@ember/service";
 import { getOwner } from "@ember/application";
+import { action } from "@ember/object";
+import { service } from "@ember/service";
 
 export default class DiscourseTagBanners extends Component {
   @service store;
